3a2114b82e09e4c02facbf314e6d3b5baee63aa4
Rohit Kumar <rohitdotnet@outlook.com>
2021-08-19 16:36:36 +0530

    Modify first comp

--- force-app/main/default/lwc/firstComp/firstComp.js
+++ force-app/main/default/lwc/firstComp/firstComp.js
@@ -1,3 +1,16 @@
+/**
+ * @description       : 
+ * @author            : Rohit Kumar
+ * @group             : 
+ * @last modified on  : 08-19-2021
+ * @last modified by  : Rohit Kumar
+ **/
 import { LightningElement } from 'lwc';
 
-export default class FirstComp extends LightningElement {}
+export default class FirstComp extends LightningElement {
+
+
+    msg = "This is heading"
+
+
+}
